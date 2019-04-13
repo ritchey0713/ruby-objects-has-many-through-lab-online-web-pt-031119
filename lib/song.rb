e1d5c396173348be62b428d5f65f07c4014fdde2
David Ritchey <ritchey0713@gmail.com>
2019-04-13 00:54:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,12 @@
 class Song 
   attr_accessor :name 
-  
+    @@all = []
   def initialize(name)
-    @name = name 
+    @name = name
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
   end 
 end 
